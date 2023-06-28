1db81472811dc3373259e318e2c9242d5d9d2524
Gizachew Bayness <eleccrazy24@gmail.com>
2023-06-28 14:15:43 +0300

    Add TypeScript type to ProgramCard component props

--- client/src/pages/Customization.tsx
+++ client/src/pages/Customization.tsx
@@ -1,25 +1,45 @@
 import { Box, Grid, Typography } from '@mui/material';
-import CustomCard from '../components/common/CustomCard';
 import ProgramCard from '../components/common/ProgramCard';
 
 const Customization = () => {
   return (
     <Box>
-      <Typography fontSize={25} fontWeight={700} sx={{mb: '15px'}} color='#11142D'>
+      <Typography
+        fontSize={25}
+        fontWeight={700}
+        sx={{ mb: '15px' }}
+        color='#11142D'
+      >
         Customization
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <ProgramCard/>
+          <ProgramCard
+            title='title one'
+            summary='summary one'
+            image='image one'
+          />
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <ProgramCard/>
+          <ProgramCard
+            title='title two'
+            summary='summary two'
+            image='image two'
+          />
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <ProgramCard/>
+          <ProgramCard
+            title='title three'
+            summary='summary three'
+            image='image three'
+          />
         </Grid>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <ProgramCard/>
+          <ProgramCard
+            title='title four'
+            summary='summary four'
+            image='image four'
+          />
         </Grid>
       </Grid>
     </Box>
