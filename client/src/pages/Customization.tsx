2565c6eab10aca7e8258d69dceefecb5d0df9a1a
Helina Abye <helinaago@gmail.com>
2023-06-28 13:47:20 +0300

    add cards for customization options

--- client/src/pages/Customization.tsx
+++ client/src/pages/Customization.tsx
@@ -1,10 +1,27 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
+import CustomCard from '../components/common/CustomCard';
+import ProgramCard from '../components/common/ProgramCard';
+
 const Customization = () => {
   return (
     <Box>
-      <Typography fontSize={25} fontWeight={700} color='#11142D'>
+      <Typography fontSize={25} fontWeight={700} sx={{mb: '15px'}} color='#11142D'>
         Customization
       </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <ProgramCard/>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <ProgramCard/>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <ProgramCard/>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <ProgramCard/>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
