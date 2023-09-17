545d8a180c971b313f30caf64acdd58a1eb39182
Gizachew Bayness <eleccrazy24@gmail.com>
2023-09-17 20:50:51 +0300

    Fixing the top margin from the guest and learner details. removing unnecessary top margins

--- client/src/pages/GuestDetails.tsx
+++ client/src/pages/GuestDetails.tsx
@@ -7,14 +7,7 @@ import { Box } from '@mui/material';
 
 const GuestDetails = () => {
   return (
-    <Box
-      mt={7}
-      mb={5}
-      display='flex'
-      flexDirection='column'
-      bgcolor='#ffffff'
-      p={3}
-    >
+    <Box mb={5} display='flex' flexDirection='column' bgcolor='#ffffff' p={3}>
       <StudentProfile type='guests' />
       <AttendanceSection />
     </Box>
--- client/src/pages/StudentDetails.tsx
+++ client/src/pages/StudentDetails.tsx
@@ -7,14 +7,7 @@ import { Box } from '@mui/material';
 
 const StudentDetails = () => {
   return (
-    <Box
-      mt={7}
-      mb={5}
-      display='flex'
-      flexDirection='column'
-      bgcolor='#ffffff'
-      p={3}
-    >
+    <Box mb={5} display='flex' flexDirection='column' bgcolor='#ffffff' p={3}>
       <StudentProfile type='learners' />
       <AttendanceSection />
     </Box>
